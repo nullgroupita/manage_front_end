8e6389a7ae5825f6725fce235fa7986ecff2324f
Akin <akin.gan@oocl.com>
2019-07-29 19:20:40 +0800

    [Akin][feat: add the login page and logic & add the vue-cookies for store the token information]

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,68 @@
 import axios from 'axios'
+import cookies from 'vue-cookies'
+import router from '../router'
+import {Message} from 'element-ui'
+import md5 from 'md5'
+
 axios.defaults.baseURL = '/api'
 
+axios.interceptors.request.use(
+  config => {
+    config.baseURL = 'http://18.179.142.236:9000'
+    config.withCredentials = true
+    config.timeout = 5000
+    let token = cookies.get('token')
+    if (token) {
+      config.headers = {
+        'token': token
+      }
+    }
+    return config
+  },
+  error => {
+    return Promise.reject(error)
+  }
+)
+
+axios.interceptors.response.use(
+  response => {
+    if (response.data.retCode === 501) {
+      router.push('login')
+      Message.error('请先登录')
+    }
+    return response
+  },
+  error => {
+    return Promise.reject(error)
+  }
+)
+
+async function login (params) {
+  try {
+    params.password = md5(params.password)
+    const response = await axios.post('/login', params)
+    if (response.data.retCode === 200) {
+      // set token
+      cookies.set('token', response.data.data)
+      return true
+    }
+    return false
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+async function getLoginUserInformation () {
+  try {
+    const response = await axios.get('/employees/0')
+    return response.data.data
+  } catch (e) {
+    console.log(e)
+  }
+}
+
 const api = {
+  login,
+  getLoginUserInformation
 }
 export default api
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '../views/Home'
+import Login from '../views/Login'
 
 Vue.use(Router)
 
@@ -8,6 +9,11 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/home',
       name: 'Home',
       component: Home,
       children: [

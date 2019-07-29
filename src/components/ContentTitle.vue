c159aedcdcef679b4512fcdc59a134556ef7fb37
Akin <akin.gan@oocl.com>
2019-07-29 17:23:23 +0800

    [Akin][feat:  add the ContentTitle component and set the change of menu information to store]

---
+++ src/components/ContentTitle.vue
@@ -0,0 +1,58 @@
+<template>
+  <el-row style="background-color: #FFFFFF; margin-bottom: 10px;">
+    <el-row class="parent-title">{{parentTitle}}</el-row>
+    <el-row>
+      <hr size="2" color="#e4e7ec">
+    </el-row>
+    <el-row class="sub-title">
+      {{subTitle}}
+    </el-row>
+  </el-row>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      parentTitle: '',
+      subTitle: ''
+    }
+  },
+  computed: {
+    menuInformation () {
+      return this.$store.state.activeMenu
+    }
+  },
+  watch: {
+    menuInformation (newVal) {
+      this.parentTitle = newVal.parentTitle
+      this.subTitle = newVal.subTitle
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+  .parent-title {
+    font-weight: 400;
+    font-size: 20px;
+    color: rgb(112, 112, 112);
+    line-height: 29px;
+    text-decoration: none;
+    text-align: left;
+    padding-left: 20px;
+  }
+
+  .sub-title {
+    font-weight: 700;
+    font-size: 18px;
+    color: rgb(16, 16, 16);
+    font-style: normal;
+    letter-spacing: 0px;
+    line-height: 26px;
+    text-decoration: none;
+    text-align: left;
+    padding-left: 10px;
+  }
+</style>
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,4 +1,10 @@
+import {CHANGE_ACTIVE_MENU, MENU_LIST} from '../common/constants'
+
 const mutations = {
+  [CHANGE_ACTIVE_MENU] (state, data) {
+    let menuInformation = MENU_LIST.filter(value => value.key === data)
+    state.activeMenu = menuInformation[0]
+  }
 }
 
 export default mutations

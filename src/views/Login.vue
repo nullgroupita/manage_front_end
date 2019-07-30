5bcdabd899333512fcc72d5f7ec51803a0e51091
KloeAn <KLOE.AN@OOCL.COM>
2019-07-30 09:31:08 +0800

    [Kloe][fix: fix the login page UI]

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,9 @@
 <template>
-<el-row>
+<el-row :gutter="20">
+  <el-col :span = 6 :offset="9">
+    <img src="../assets/img/Parking_logo.svg" class="logo-icon">
+  </el-col>
+  <el-col :span = 6 :offset="9">
   <el-form ref="form" label-width="80px">
     <el-form-item label="用户名">
       <el-input v-model="username"></el-input>
@@ -7,9 +11,12 @@
     <el-form-item label="密码">
       <el-input type="password" v-model="password"></el-input>
     </el-form-item>
+  </el-form>
+  </el-col>
+  <el-col :span = 4 :offset="11">
     <el-button type="primary" @click="onLogin">登录</el-button>
     <el-button type="reset" @click="onReset">重置</el-button>
-  </el-form>
+  </el-col>
 </el-row>
 </template>
 
@@ -45,4 +52,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .el-row {
+    margin-top: 200px;
+  }
+
+  .logo-icon {
+    width: 30%;
+    margin-left: 35%;
+    margin-bottom: 50px;
+  }
 </style>
